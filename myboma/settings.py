4bc721af0fb4a9f51fac4e1f05dbd27a207dfd21
Dindihub <100135497+Dindihub@users.noreply.github.com>
2023-01-22 18:07:59 +0300

    Set hosts

--- myboma/settings.py
+++ myboma/settings.py
@@ -62,7 +62,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['boma-watch-production.up.railway.app']
 
 
 # Application definition
